c916306a7029c6d43b89ac23723d88e04e50295e
arun <jkarun63@gmail.com>
2017-05-17 18:48:20 +0530

    initial commit

---
+++ new app/Curd/src/main/java/com/curd/model/Employee.java
@@ -0,0 +1,30 @@
+package com.curd.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int empid;
+	private String name;
+	
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
